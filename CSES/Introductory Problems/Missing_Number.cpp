e6a3d521d4009d72bd29f179c8612987bf22e0a5
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-01-08 22:50:30 +0600

    Add files via upload

---
+++ CSES/Introductory Problems/Missing_Number.cpp
@@ -0,0 +1,28 @@
+//In the name of Almighty Allah
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define mod 1e9+7
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define case(z) cout<<"Case "<<z<<": "
+#define ll long long
+#define nl "\n"
+#define NO cout << "NO" << endl;
+#define Yes cout << "Yes" << endl;
+#define No cout << "No" << endl;
+#define yes cout << "yes" << endl;
+#define no cout << "no" << endl;
+#define YES cout << "YES" << endl;
+int main()
+{
+    fast;
+    ll n,i,j,sum=0;
+    cin>>n;
+    for(i=1;i<n;i++)
+    {
+      cin>>j;
+      sum+=j;
+    }
+    cout<<((n*(n+1))/2)-sum<<endl;
+  return 0;
+}
