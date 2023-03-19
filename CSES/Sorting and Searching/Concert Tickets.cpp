db7534b43a2eb87fc53dff2d4cff704b5d24ac5d
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-03-19 14:47:37 +0600

    Add files via upload

---
+++ CSES/Sorting and Searching/Concert Tickets.cpp
@@ -0,0 +1,57 @@
+//In the name of Almighty Allah
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define mod 1e9+7
+
+#define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+
+#define case(z) cout<<"Case "<<z<<": "
+
+#define ll long long
+
+#define nl "\n"
+
+#define NO cout << "NO" << endl;
+
+#define Yes cout << "Yes" << endl;
+
+#define No cout << "No" << endl;
+
+#define yes cout << "yes" << endl;
+
+#define no cout << "no" << endl;
+
+#define YES cout << "YES" << endl;
+ll gcd(ll a,ll b){
+	 return __gcd(a,b);
+}
+
+ll lcm(ll a,ll b){
+	 return (a*(b/gcd(a,b)));
+}
+const int mx=2e5+123;
+int main()
+{
+       fast;
+       int t,i,x,n,m;
+       cin>>n>>m;
+       multiset<int,greater<int>>s;
+      for(i=0;i<n;i++){
+        cin>>x;
+        s.insert(x);
+      }
+      while(m--)
+      {
+        cin>>x;
+        auto it=s.lower_bound(x);
+        if(it==s.end())
+        cout<<-1<<nl;
+        else{
+           cout<<(*it)<<nl;
+           s.erase(it);
+        }
+      }
+    return 0;
+}
