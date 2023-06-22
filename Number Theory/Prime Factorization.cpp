d933cac056e30627160af9282caae8fbafa1f643
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-06-22 12:27:22 +0600

    Add files via upload

---
+++ Number Theory/Prime Factorization.cpp
@@ -0,0 +1,72 @@
+//In the name of Almighty Allah
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define nl '\n'
+#define PB push_back
+#define F first
+#define S second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+ 
+ 
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+const int mx=123;
+int a[mx];
+void primeFactor(int n)
+{     
+      for(int i=2;i*i<=n;i++)
+       {
+         if(n%i==0)
+         {
+           int cnt=0;
+           while(n%i==0)
+            cnt++,n/=i;
+            cout<<i<<"^"<<cnt<<nl;
+         }
+       }
+      if(n>1)cout<<n<<' '<<1<<nl;
+}
+int main()
+
+{
+       fast;
+       int t,i,j,x,g=0,type,n,k,diff,m;
+       cin>>n;
+       primeFactor(n);
+    return 0;
+}
