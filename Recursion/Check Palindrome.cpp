d47ef8759cb4fde52919847f2fc0693321016475
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-07-09 16:36:39 +0600

    Add files via upload

---
+++ Recursion/Check Palindrome.cpp
@@ -0,0 +1,75 @@
+//In the name of Almighty Allah
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define nl '\n'
+#define pb push_back
+#define f first
+#define s second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define mod 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+ll pow(ll a,ll n)
+{     
+      ll res=1;
+      while(n)
+      {
+        if(n%2)res*=a,n--;
+        else a*=a,n/=2;
+      }
+      return res;
+}
+ 
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+const int mx=123;
+bool Palindrom(int i,string s,int n)
+{
+  if(i >= n/2)return true;
+  if(s[i]!=s[n-i-1])
+   return false;
+  return Palindrom(i+1,s,n);
+}
+int main()
+
+{
+       fast;
+       int t,i,j,x,g,type,n,k,diff,m;
+       string s;
+       cin>>n>>s;
+       cout<<(Palindrom(0,s,n)?"Palindrome":"Not Palindrome")<<nl;
+    return 0;
+}
