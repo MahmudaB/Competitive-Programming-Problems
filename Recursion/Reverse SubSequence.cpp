701ab67060e92d05c5e2f5df3a5906c02b51caa7
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-07-13 23:53:51 +0600

    Add files via upload

---
+++ Recursion/Reverse SubSequence.cpp
@@ -0,0 +1,28 @@
+//In the name of Almighty Allah
+#include<bits/stdc++.h>
+using namespace std;
+
+void Reverse_Subseq(int i,vector<int> &v, int a[],int n)
+{
+  if(i>=n)
+  {
+    if(v.size()==0)cout<<"{}";
+    for(auto x:v)
+     cout<<x<<' ';
+    cout<<endl;
+    return;
+  }
+  Reverse_Subseq(i+1,v,a,n);
+  
+  v.push_back(a[i]);
+  Reverse_Subseq(i+1,v,a,n);
+  v.pop_back();
+}
+int main()
+{
+
+       int a[]={3,1,2,4},n=4;
+       vector<int>v;
+       Reverse_Subseq(0,v,a,n);
+    return 0;
+}
