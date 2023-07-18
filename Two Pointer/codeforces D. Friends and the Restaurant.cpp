066b9ff46d80d409cbd2aae34c95fb4535e55dbd
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-07-18 22:52:18 +0600

    Add files via upload

---
+++ Two Pointer/codeforces D. Friends and the Restaurant.cpp
@@ -0,0 +1,89 @@
+//In the name of Almighty Allah
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+ 
+#define nl '\n'
+#define pb push_back
+#define f first
+#define s second
+#define all(a) (a).begin(),(a).end()
+#define rall(a) (a).rbegin(),(a).rend()
+#define sz(x) (int)x.size()
+ 
+const double PI = acos(-1);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define mod 1000000007
+ 
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define sqr(a) ((a) * (a))
+ 
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+#define file() freopen("input.txt","r",stdin);freopen("output.txt","w",stdout);
+
+ll pow(ll a,ll n)
+{     
+      ll res=1;
+      while(n)
+      {
+        if(n%2)res*=a,n--;
+        else a*=a,n/=2;
+      }
+      return res;
+}
+ 
+ll gcd ( ll a, ll b )
+{
+    return __gcd ( a, b );
+}
+ll lcm ( ll a, ll b )
+{
+    return a * ( b / gcd ( a, b ) );
+}
+
+const int mx=1e5+123;
+ll a[mx],b[mx];
+int main()
+
+{
+       fast;
+       ll t,i,j,x,type,n,k,diff,m;
+       cin>>t;
+       while(t--)
+       { 
+         cin>>n;
+         for(i=1;i<=n;i++)
+          cin>>a[i];
+         for(i=1;i<=n;i++)
+          cin>>b[i];
+          
+        vl v;
+         for(i=1;i<=n;i++)
+          v.pb(b[i]-a[i]);
+         
+         sort(all(v));
+         ll cnt=0;
+         for(i=0,j=n-1;i<j;)
+         {
+           if(v[j]+v[i]>=0)i++,j--,cnt++;
+           else i++;
+         }
+         cout<<cnt<<nl;
+       }
+        
+    return 0;
+}
