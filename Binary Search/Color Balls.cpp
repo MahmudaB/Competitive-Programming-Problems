d23843069fcabda44c069219aa897b58d7115a25
Mahmuda Begum <98878415+MahmudaB@users.noreply.github.com>
2023-11-12 14:17:44 +0600

    Color Balls.cpp

--- Binary Search/Color Balls.cpp
+++ Binary Search/Color Balls.cpp
@@ -1,4 +1,42 @@
 //In the name of Almighty Allah
+/*
+Description
+
+You are given N balls and an integer K. The color of each ball is given in an array A. A basket is filled if it contains at least K balls of different colors. Find the maximum number of filled baskets you can get if you optimally put the balls in the baskets.
+
+
+Input Format
+
+The first line contains T, the number of test cases (1<=T<=100000).
+
+The first line contains three space-separated integers N, K where 1<=N<=10^5, 1<=K<=N.
+
+Next line contains N space-separated integers (0<=Ai<=1e9).
+
+Sum of N across all test cases <=10^6.
+
+
+Output Format
+
+For each test case output the maximum number of filled baskets in a new line. 
+
+
+Sample Input 1
+
+3
+5 5
+1 2 3 4 5
+5 2
+1 2 3 4 5
+6 3
+1 2 2 1 3 3
+
+Sample Output 1
+
+1
+2
+2
+*/
 #include<bits/stdc++.h>
 using namespace std;
  
